e1a44ad0fb18a66b8619848713552bfbd2ca7f3e
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2017-09-13 09:40:56 +0900

    dddd

---
+++ Assets/MyGameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MyGameManager {
+
+    private static MyGameManager _instance;
+
+    public static MyGameManager Get() {
+        if (_instance == null)
+            _instance = new MyGameManager();
+        return _instance;
+    }
+
+    private List<MapData> _mapData = new List<MapData>();
+
+    public void Initialize()
+    {
+
+    }
+}
---
+++ Assets/Scene_Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene_Main : MonoBehaviour {
+
+    [Header("Stage Grid")]
+    public GameObject stageGrid;
+
+    private void Awake() {
+        MyGameManager.Get().Initialize();
+
+        stageGrid.SetActive(false);
+    }
+
+    public void SetStageGrid() {
+        stageGrid.SetActive(true);
+    }
+}
