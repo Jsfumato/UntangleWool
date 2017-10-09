de74b6530f22cce0ab6d8517daf9cb907cc61732
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2017-10-10 05:03:50 +0900

    dddd

---
+++ UntangleMeow/Assets/Scripts/Common/AdsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+
+    private string platformGameID;
+
+	// Use this for initialization
+	void Awake() {
+#if UNITY_EDITOR || UNITY_ANDROID
+        platformGameID = "1557491";
+#elif UNITY_IOS
+        platformGameID = "1557490";
+#endif
+        Advertisement.Initialize(platformGameID);
+    }
+}
