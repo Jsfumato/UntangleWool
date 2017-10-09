e16e94d8d1d8c94ba972574200a761cdc0c16e39
HUAWEI\JS <jsfumato@naver.com>
2017-10-09 12:22:01 +0900

    dddd

---
+++ Assets/Scripts/Common/Localizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class Localizer
+{
+    private static Localizer _instance;
+
+    private TextAsset localizerXml;
+
+    public Localizer Get()
+    {
+        if (_instance == null)
+            _instance = new Localizer();
+
+        return _instance;
+    }
+
+	void Awake()
+    {
+        ImportLocalizerData();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ImportLocalizerData()
+    {
+        localizerXml = Resources.Load<TextAsset>("Localizer");
+        var doc = XDocument.Parse(localizerXml.text);
+
+        foreach (var stage in doc.Element("Stages").Elements("Stage"))
+            mapDataList.Add(new MapData(stage));
+    }
+}
---
+++ Assets/Scripts/Common/Utility.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using UnityEngine;
+
+//public class Utility : MonoBehaviour {
+
+	
+//}
+
+public static class XElementExtensions
+{
+    public static XmlNode ToXmlNode(this XElement element)
+    {
+        if (element == null)
+            return null;
+
+        XmlNode node = null;
+        using (var reader = element.CreateReader())
+        {
+            var xml = new XmlDocument();
+            xml.Load(reader);
+
+            node = xml.DocumentElement;
+        }
+        return node;
+    }
+}
+
+public static class ResourceExtensions
+{
+    public static long GetAttributeLong(this XElement element, string AttrName, long defaultValue = long.MinValue)
+    {
+        if (element.Attribute(AttrName) == null)
+            return defaultValue;
+
+        long ret;
+        if (long.TryParse(element.Attribute(AttrName).Value, out ret) == false)
+            return defaultValue;
+
+        return ret;
+    }
+
+    public static float GetAttributeFloat(this XElement element, string AttrName, float defaultValue = float.NaN)
+    {
+        if (element.Attribute(AttrName) == null)
+            return defaultValue;
+
+        float ret;
+        if (float.TryParse(element.Attribute(AttrName).Value, out ret) == false)
+            return defaultValue;
+
+        return ret;
+    }
+}
